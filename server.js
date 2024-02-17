6b41b76449b175c6243f2ae0e6bca1532fb848b5
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-17 21:02:00 +0530

    pdf4

--- server.js
+++ server.js
@@ -2,23 +2,32 @@ import express from 'express';
 import multer from 'multer';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import merge from '../PdfMerger/merger/merge.js';
+
+
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const upload = multer({ dest: 'uploads/' })
+
 const app = express();
 
-const port = 3000;
-
+const port = 3001;
+app.use('/static', express.static('public'))
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'templates', 'index.html'));
+    
+    
+      res.sendFile(path.join(__dirname, 'templates', 'index.html'));
 });
 
-app.post('/merge', upload.array('pdfs', 2), function (req, res, next) {
+app.post('/merge', upload.array('pdfs', 2), async (req, res, next)=> {
     // req.files is array of `photos` files
     // req.body will contain the text fields, if there were any
     console.log(req.files)
-    res.send({data: req.files})
+    await merge(path.join(__dirname, req.files[0].path), path.join(__dirname, req.files[1].path))
+    res.redirect("http://localhost:3001/static/merged.pdf")
+
+
 })
   
 
