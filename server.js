8218fc69cb79620736111068f9a76d5ece153356
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2024-02-17 19:57:15 +0530

    pdf1

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import express from 'express';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const app = express();
+
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile(path.join(__dirname, 'templates', 'index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port http://localhost:${port}`);
+});
